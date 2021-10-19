4bb4cb8fd7bb8469ea4015e36278ba64b05fd082
Rūta Buckiūnaitė <48050553+Aneliukee@users.noreply.github.com>
2021-10-20 00:43:17 +0300

    Add files via upload

---
+++ Client/ClientClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Client
+{
+    public class ClientClass
+    {
+		public ClientClass()
+		{
+
+		}
+
+		public string Output(string key)
+		{
+			Common.IData DataMarshal = (Common.IData)Activator.GetObject(
+			typeof(Common.IData), "tcp://localhost:8001/DataMarshal.rem");
+
+			if (key == "")
+			{
+				return "Error!";
+			}
+			else
+			{
+				Common.Data dMarshal = DataMarshal.Object(key);
+				return dMarshal.ToString();
+			}
+		}
+	}
+}
